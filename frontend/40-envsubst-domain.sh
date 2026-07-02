c709f2ba11b85a2b0b2e5a6f56f3576bd8d2b044
Roman <rpetrov2006@mail.ru>
2026-07-02 10:13:46 +0700

    fix script

--- frontend/40-envsubst-domain.sh
+++ frontend/40-envsubst-domain.sh
@@ -3,3 +3,5 @@ set -e
 
 export DOMAIN="${DOMAIN:-lms.signal.qlabs.pro}"
 envsubst '$DOMAIN' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+
+( while :; do sleep 6h; nginx -s reload; done ) &
